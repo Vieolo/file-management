ce074d531ffee42a366a0f5063449c09d1acd884
Ramtin <Ramtin.abadi@gmail.com>
2022-10-12 22:59:12 +0530

    Added download file to disk function

--- download.go
+++ download.go
@@ -4,8 +4,52 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io"
+	"io/fs"
 	"net/http"
+	"os"
 )
+
+// Downloads the file from `fileURL` and writes to the disk
+// The new file will be named as `diskFileName` and will be saved to `diskFilePath`
+// If the permission of the file should be changed, it can be passes as `chmodPermission`, e.g. 0777
+func DownloadFileToDisk(diskFilePath string, diskFileName, fileURL string, chmodPermission *fs.FileMode) (string, string, error) {
+
+	// Create the file
+	out, err := os.Create(diskFilePath + diskFileName)
+	if err != nil {
+		return "", "", err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(fileURL)
+	if err != nil {
+		return "", "", err
+	}
+	defer resp.Body.Close()
+
+	// Check server response
+	if resp.StatusCode != http.StatusOK {
+		return "", "", fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	// Writer the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return "", "", err
+	}
+
+	fPath := diskFilePath + diskFileName
+
+	if chmodPermission != nil {
+		chmodErr := os.Chmod(fPath, *chmodPermission)
+		if chmodErr != nil {
+			return "", "", chmodErr
+		}
+	}
+
+	return diskFileName, diskFilePath + diskFileName, nil
+}
 
 func DownloadFileToBytes(fileURL string) ([]byte, error) {
 	resp, err := http.Get(fileURL)
